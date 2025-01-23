1b8427b2ec4929c759d41af3af62f09e36959d13
Filippo Mangione <filippomangione@users.noreply.github.com>
2025-01-23 11:55:40 +0000

    chore: vite server.allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["6mmrl8-5173.csb.app"],
+    allowedHosts: true,
   },
 });
